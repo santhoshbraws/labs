4c8d0e8d1cbcf8b3a9a5410667e5e78bf189526b
root <root@ip-172-31-34-103.us-east-2.compute.internal>
2019-11-03 06:55:04 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+# Credentials to access aws cluster
+#  access_key = "${var.access_key}"
+#  secret_key = "${var.secret_key}"
+   region = "${var.region}"
+}
+
+resource "aws_instance" "web_server" {
+  instance_type = "${var.instance_type}"
+  ami           = "${var.ami}"
+  count         = 1
+  subnet_id     = "${var.subnet_id}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable access_key {}
+variable secret_key {}
+variable region {}
+variable instance_type {}
+variable ami {}
+variable associate_public_ip_address {}
+variable subnet_id {}
