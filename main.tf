de35f482d14a2c41baa212eb775c0bc73b39bf7d
root <root@ip-172-31-23-128.us-east-2.compute.internal>
2019-11-04 09:21:03 +0000

    mains.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 # Credentials to access aws cluster
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-# region = "${var.region}"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "web_server" {
