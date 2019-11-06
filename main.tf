957e788196050039612a35c8b52cdea8889ee767
root <root@ip-172-31-44-112.us-east-2.compute.internal>
2019-11-06 05:32:43 +0000

    mains

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ provider "aws" {
 # Credentials to access aws cluster
 #  access_key = "${var.access_key}"
 #  secret_key = "${var.secret_key}"
-   region = ${var.region}
+   region = "${var.region}"
 #  shared_credentials_file = "/var/lib/jenkins/secrets/creds"
 }
 
 resource "aws_instance" "web_server" {
-  instance_type = ${var.instance_type}
-  ami           = ${var.ami}
+  instance_type = "${var.instance_type}"
+  ami           = "${var.ami}"
   count         = 1
-  subnet_id     = ${var.subnet_id}
-  associate_public_ip_address = ${var.associate_public_ip_address}
+  subnet_id     = "${var.subnet_id}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
 }
